c4a38eb87917e3dae3ed805e723561ceaa426852
n1ppers <specialforgames251@gmail.com>
2019-02-27 02:50:10 +0300

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# vk album parser
+# A parsing script for VK social network.
+
+### Requirements:
+- Python 3
+- https://github.com/python273/vk_api
+
+### Configuration:
+When you run this script first time, it create a configuration file with 3 parameters:
+- Login
+- Password
+- Group_URL
+
+1. In "login", you need to put your VK login, or phone number which linked to your VK account.
+2. In "Password", you need to put your VK password.
+3. In "Group_URL", you need to put a URL of the group from whose albums you want to parse photos without "https://vk.com/"
+
+### Usage:
+1. Fill "Configuration.ini"
+2. Run the script.
+3. Profit! Parsed albums will be saved at "parsed" directory.
---
+++ b1g_parser.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import vk_api
+import requests
+import os
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+ 
+config = configparser.ConfigParser()
+
+def createConfig(path):
+    config.add_section("Configuration")
+    config.set("Configuration", "Login", "login")
+    config.set("Configuration", "Password", "password")
+    config.set("Configuration", "Group_URL", "daydream325")
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+config.read("Configuration.ini")
+if __name__ == "__main__":
+    path = "Configuration.ini"
+    if not os.path.exists(path):
+        createConfig(path)
+
+LOGIN = config.get("Configuration", "Login")
+PASSWORD = config.get("Configuration", "Password")
+GROUP_URL = config.get("Configuration", "Group_URL")
+
+def authorization():
+	vk_session = vk_api.VkApi(LOGIN, PASSWORD)
+	try:
+		vk_session.auth()
+	except vk_api.AuthError as error_msg:
+		print(error_msg)
+		return
+	return vk_session.get_api()
+
+def main():
+    vk = authorization()
+    group = vk.groups.getById(group_id=GROUP_URL)[0]['id']
+    photoAlbums = vk.photos.getAlbums(owner_id = "-"+str(group))['items']
+    for album in photoAlbums:
+        i = 0
+        #test = str(album['id'])
+        uff = 999 + 999 / 2 * 100
+        if not os.path.exists("parsed/"+album['title']):
+            os.makedirs("parsed/"+album['title'])
+        photos = vk.photos.get(owner_id = "-"+str(group), album_id = album['id'], photo_sizes = 1, count = 1000)['items']
+        for photo in photos:
+            try:
+                biggest = photo['sizes'][0]['width']
+                biggestSrc = photo['sizes'][0]['url']
+            except IndexError:
+                continue
+            for size in photo['sizes']:
+                if size['width'] > biggest:
+                    try:
+                        biggest = size['width']
+                        biggestSrc = size['url']
+                    except IndexError:
+                        continue
+            content = requests.get(biggestSrc).content
+            try:
+                with open("parsed/"+album['title']+"/"+str(i)+".jpg", "wb") as f:
+                    f.write(content)
+            except FileNotFoundError:
+                continue
+            print("parsed/"+album['title']+"/"+str(i)+".jpg")
+            i = i + 1
+
+if __name__ == "__main__":
+    main()
