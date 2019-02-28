8f56b4f94b31b36db43db6fd4068a1f7c2e5b955
n1ppers <specialforgames251@gmail.com>
2019-02-28 17:24:33 +0300

    added 'offset'.
    and fixed some crashes.

--- README.md
+++ README.md
@@ -5,14 +5,16 @@
 - https://github.com/python273/vk_api
 
 ### Configuration:
-When you run this script first time, it create a configuration file with 3 parameters:
+When you run this script first time, it create a configuration file with 4 parameters:
 - Login
 - Password
 - Group_URL
+- Offset
 
 1. In "login", you need to put your VK login, or phone number which linked to your VK account.
 2. In "Password", you need to put your VK password.
 3. In "Group_URL", you need to put a URL of the group from whose albums you want to parse photos without "https://vk.com/"
+4. By changing "Offset" you can 'bypass' the 1000 photos limit per request. Just increase it by 1000 every time after you will parse 1000 photos.
 
 ### Usage:
 1. Fill "Configuration.ini"
--- b1g_parser.py
+++ b1g_parser.py
@@ -14,6 +14,7 @@ def createConfig(path):
     config.set("Configuration", "Login", "login")
     config.set("Configuration", "Password", "password")
     config.set("Configuration", "Group_URL", "daydream325")
+    config.set("Configuration", "Offset", "0")
     with open(path, "w") as config_file:
         config.write(config_file)
 
@@ -26,6 +27,7 @@ if __name__ == "__main__":
 LOGIN = config.get("Configuration", "Login")
 PASSWORD = config.get("Configuration", "Password")
 GROUP_URL = config.get("Configuration", "Group_URL")
+OFFSET = config.get("Configuration", "Offset")
 
 def authorization():
 	vk_session = vk_api.VkApi(LOGIN, PASSWORD)
@@ -41,29 +43,45 @@ def main():
     group = vk.groups.getById(group_id=GROUP_URL)[0]['id']
     photoAlbums = vk.photos.getAlbums(owner_id = "-"+str(group))['items']
     for album in photoAlbums:
-        i = 0
-        if not os.path.exists("parsed/"+album['title']):
-            os.makedirs("parsed/"+album['title'])
-        photos = vk.photos.get(owner_id = "-"+str(group), album_id = album['id'], photo_sizes = 1, count = 1000)['items']
+        i = int(OFFSET)
+        try:
+            if not os.path.exists("parsed/"+album['title']):
+                os.makedirs("parsed/"+album['title'])
+        except (FileNotFoundError, NotADirectoryError, OSError):
+            if not os.path.exists("parsed/"+str(album['id'])):
+                os.makedirs("parsed/"+str(album['id']))
+        getphotos = vk.photos.get(owner_id = "-"+str(group), album_id = album['id'], photo_sizes = 1, count = 1000, offset = OFFSET)
+        photos = getphotos['items']
         for photo in photos:
             try:
-                biggest = photo['sizes'][0]['width']
-                biggestSrc = photo['sizes'][0]['url']
+                if int(OFFSET) < int(getphotos['count']):
+                    biggest = photo['sizes'][0]['width']
+                    biggestSrc = photo['sizes'][0]['url']
+                else:
+                    continue
             except IndexError:
                 continue
             for size in photo['sizes']:
                 if size['width'] > biggest:
                     try:
-                        biggest = size['width']
-                        biggestSrc = size['url']
+                        if int(OFFSET) < int(getphotos['count']):
+                            biggest = size['width']
+                            biggestSrc = size['url']
+                        else:
+                            continue
                     except IndexError:
                         continue
             content = requests.get(biggestSrc).content
             try:
                 with open("parsed/"+album['title']+"/"+str(i)+".jpg", "wb") as f:
                     f.write(content)
-            except FileNotFoundError:
-                continue
+            except (FileNotFoundError, NotADirectoryError, OSError):
+                try:
+                    with open("parsed/"+str(album['id'])+"/"+str(i)+".jpg", "wb") as f:
+                        f.write(content)
+                except FileNotFoundError:
+                    continue
+            
             print("parsed/"+album['title']+"/"+str(i)+".jpg")
             i = i + 1
 
